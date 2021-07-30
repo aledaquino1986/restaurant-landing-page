ee339d0a22fb56afac706cd19cdfec32c43da496
alejandro <alejandrodanielaquino@yahoo.com.ar>
2021-07-30 00:27:21 -0300

    fix responsive design issues

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 window.addEventListener("scroll", () => {
-const header = document.querySelector("header")
-header.classList.toggle("sticky", window.scrollY > 0)
-})
+  const header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 0);
+});
 
+const menuToggle = document.querySelector(".menu-toggle");
+const nav = document.querySelector("nav");
+const listItems = document.querySelectorAll(".list-item");
 
+menuToggle.addEventListener("click", () => {
+  menuToggle.classList.toggle("active");
+  nav.classList.toggle("active");
+});
 
-
+listItems.forEach(listItem => {
+  listItem.addEventListener("click", () => {
+    menuToggle.classList.remove("active");
+    nav.classList.remove("active");
+  });
+});
